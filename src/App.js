47cad282517f4d718625c14fe6e5fcb69fad0c76
Bhanu Prakash Reddy <126443511+em-tpt-bbandi@users.noreply.github.com>
2024-04-02 12:17:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function FormTodo({ addTodo }) {
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group>
-        <Form.Label><b>Add Todo</b></Form.Label>
+        <Form.Label><b>Add Todo Bhanu Bandi</b></Form.Label>
         <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
       </Form.Group>
       <Button variant="primary mb-3" type="submit">
